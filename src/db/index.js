0af8722d7e0fb24e4a7d8fa22520243a5937746c
Amar <42346300+amarmaulana95@users.noreply.github.com>
2025-08-28 14:46:07 +0700

    config env test

--- src/db/index.js
+++ src/db/index.js
@@ -1,6 +1,11 @@
 const { Pool } = require('pg');
+const dotenv = require('dotenv');
+
+const envFile = `.env.${process.env.NODE_ENV || 'dev'}`;
+dotenv.config({ path: envFile });
+
 const pool = new Pool({
-    connectionString: process.env.DATABASE_URL
+  connectionString: process.env.DATABASE_URL
 });
 
 module.exports = pool;
