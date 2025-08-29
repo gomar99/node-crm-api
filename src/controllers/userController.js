bfe1fcf74ac428aae095a59b50476f8fe8221112
Amar <42346300+amarmaulana95@users.noreply.github.com>
2025-08-29 14:42:08 +0700

    config env test

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,11 +1,12 @@
+// controllers/userController.js
 const pool = require('../db');
 
 exports.getUsers = async (req, res) => {
-    try {
-        const result = await pool.query('SELECT id, name, email, created_at FROM users');
-        res.json(result.rows);
-    } catch (err) {
-        console.error(err);
-        res.status(500).json({ error: 'Database error' });
-    }
+  try {
+    const result = await pool.query('SELECT * FROM users');
+    res.json(result.rows);
+  } catch (err) {
+    console.error("DB ERROR:", err.message); // <--- tambahin ini
+    res.status(500).json({ error: "Internal server error" });
+  }
 };
